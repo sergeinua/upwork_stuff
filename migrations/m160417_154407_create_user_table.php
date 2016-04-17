05e2acf9fe6542f5c58b35b6a3a99bedfe0e918f
Sergei <sergei.nikonov@yahoo.com>
2016-04-17 19:01:50 +0300

    user table migration added

---
+++ migrations/m160417_154407_create_user_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use yii\db\Migration;
+
+class m160417_154407_create_user_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable('user', [
+            'id' => $this->integer(11)->primaryKey(),
+            'username' => $this->string(50)->notNull(),
+            'password' => $this->string(60)->notNull(),
+            'authKey' => $this->string(60)->notNull(),
+            'created_at' => $this->string(11)->notNull(),
+            'modified_at' => $this->string(11),
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('user');
+    }
+}
